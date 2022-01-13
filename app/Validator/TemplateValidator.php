eb95d04e83d1b7e8e6fa9a5654d109a81a4e7a11
David Rico <david.rico@iudigital.edu.co>
2022-01-13 08:20:32 -0500

    TemplateValidator validator create

---
+++ app/Validator/TemplateValidator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Validator;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TemplateValidator
+{
+    /**
+     *
+     * @var Request
+     */
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function validate()
+    {
+        return Validator::make($this->request->all(), $this->rules(), $this->messages());
+    }
+
+    private function rules()
+    {
+        return [
+            "nombrePlantilla" => "required",
+            "imgCertificado" => "required",
+        ];
+    }
+
+    private function messages()
+    {
+        return [];
+    }
+}
