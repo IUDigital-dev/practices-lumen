a9a8f76aeb769f2bcf847e144735108a86d2b974
Juan Duque <juuanduuke@gmail.com>
2022-02-07 12:40:46 -0500

    reset repositorio main

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,23 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
-use Laravel\Lumen\Routing\Controller as BaseController;
-
-class Controller extends BaseController
+class Controller
 {
-    public function response($data, $status = 200)
-    {
-        return response()->json($data, $status);
-    }
 
-
-    // protected function respondWithToken($token)
-    // {
-    //     return response()->json([
-    //         'token' => $token,
-    //         'token_type' => 'bearer',
-    //         'expires_in' => Auth::factory()->getTTL() * 60
-    //     ], 200);
-    // }
 }
